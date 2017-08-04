e71456eea5f2ece7bd589482b08c850818ee3763
Mr.LuDashi <lizelusdut@qq.com>
2017-08-04 18:39:21 +0800

    update source code

--- RACWithSwift/ViewController.swift
+++ RACWithSwift/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var firstLabel: UILabel!
+    @IBOutlet var secondLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func tapStep1(_ sender: UIStepper) {
+       self.firstLabel.text = createText(count: Int(sender.value))
+    }
+    
+    @IBAction func tapStep2(_ sender: UIStepper) {
+        self.secondLabel.text = createText(count: Int(sender.value))
+    }
+    
+    func createText(count: Int) -> String {
+        var str = ""
+        for _ in 0..<count {
+            str.append("字")
+        }
+        return str
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
