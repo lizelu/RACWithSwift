9f4d73a835df4fe22ccfd9fa6f4c42540df39675
Mr.LuDashi <lizelusdut@qq.com>
2017-08-28 10:05:30 +0800

    update source code

--- RACWithSwift/ViewController.swift
+++ RACWithSwift/ViewController.swift
@@ -9,48 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var firstLabel: UILabel!
-    @IBOutlet var secondLabel: UILabel!
-    
-    @IBOutlet var bottomLabel1: UILabel!
-    @IBOutlet var bottomLabel2: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    @IBAction func tapStep1(_ sender: UIStepper) {
-       self.firstLabel.text = createText(count: Int(sender.value))
-    }
-    
-    @IBAction func tapStep2(_ sender: UIStepper) {
-        self.secondLabel.text = createText(count: Int(sender.value))
-    }
-    
-    @IBAction func tapSwitch(_ sender: UISwitch) {
-        if sender.isOn {
-            self.bottomLabel1.setContentCompressionResistancePriority(751, for: .horizontal)
-            self.bottomLabel2.setContentCompressionResistancePriority(750, for: .horizontal)
-        } else {
-            self.bottomLabel1.setContentCompressionResistancePriority(750, for: .horizontal)
-            self.bottomLabel2.setContentCompressionResistancePriority(751, for: .horizontal)
-        }
-//        self.view.setNeedsLayout()
-//        self.view.layoutIfNeeded()
-    }
-    
-    func createText(count: Int) -> String {
-        var str = ""
-        for _ in 0..<count {
-            str.append("字")
-        }
-        return str
-    }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 }
